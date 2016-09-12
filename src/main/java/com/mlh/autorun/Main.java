3cddcfbe27257ba8023562597588173a862d9dcb
songjinglei <506943940@qq.com>
2016-09-12 15:05:25 +0800

    TEST Commit

--- src/main/java/com/mlh/autorun/Main.java
+++ src/main/java/com/mlh/autorun/Main.java
@@ -2,12 +2,14 @@ package com.mlh.autorun;
 
 import org.apache.commons.lang.StringUtils;
 
+import com.mlh.enums.Confirm;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("test main...");
 		boolean eq = StringUtils.equals("a", "d");
 		System.out.println(eq);
-	}
+		System.out.println("更新为已下载：" + Confirm.yes.toString());	}
 
 }
